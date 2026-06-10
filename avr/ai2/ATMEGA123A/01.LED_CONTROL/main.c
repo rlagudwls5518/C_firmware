272c3a9e9b4a9efe40b59d23999d5029ccda44a8
rlagudwls5518 <rlagudwls5518@naver.com>
2026-06-10 12:36:48 +0900

    실습파일 추가

---
+++ avr/ai2/ATMEGA123A/01.LED_CONTROL/main.c
@@ -0,0 +1,24 @@
+﻿/*
+ * 01.LED_CONTROL.c
+ *
+ * Created: 2026-06-10 오전 10:20:29
+ * Author : kccistc
+ */ 
+#define F_CPU 16000000UL // 16MHz
+#include <avr/io.h> // PORTA PORTD 등의 I/O register들이 들어있다.
+#include <util/delay.h> // _delay_ms _delay_us 등의 함수가 들어있다.
+
+int main(void)
+{
+    
+    DDRA = 0b11111111; // PORTA에 LED가 8개 연결되어있다
+	//DDR(DAta Direction Register) 방향설정
+	// 1: 출력 0: 입력
+	while (1) {
+		PORTA = 0b11111111; // all on
+		_delay_ms(1000); // 1초 유지 1000ms : 1초 500ms : 0.5초
+		PORTA = 0b00000000; // all off
+		_delay_ms(1000); // 1초 sleep
+    }
+}
+
