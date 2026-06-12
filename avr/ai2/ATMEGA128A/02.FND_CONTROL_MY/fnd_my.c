f59e161480a82e9de494aeef9c35788cf7b3ad92
rlagudwls5518 <rlagudwls5518@naver.com>
2026-06-12 16:57:18 +0900

    0612/과제 미완

--- avr/ai2/ATMEGA128A/02.FND_CONTROL_MY/fnd_my.c
+++ avr/ai2/ATMEGA128A/02.FND_CONTROL_MY/fnd_my.c
@@ -4,24 +4,65 @@
 * Created: 2026-06-12 오전 10:52:32
 *  Author: kccistc
 */
-#include "fnd.h"
+#include "fnd_my.h"
 
 //분 초 시계만들기
+uint32_t stopwatch_ms_count = 0;
 uint32_t ms_count = 0;
 uint32_t sec_count = 0;
 uint8_t dot_display = 0;
-void init_fnd();
-void fnd_display();
-int fnd_main();
+uint32_t sec_10 = 0; // 초의 10의 자리 (0~5)
+uint32_t sec_1  = 0; // 초의 1의 자리 (0~9)
+uint32_t ms_10  = 0;  // 밀리초의 10의 자리 (0~9)
+uint32_t ms_1   = 0;  // 밀리초의 1의 자리 (0~9)
 
-int fnd_main(){
-	init_fnd();
+#if 1					//   0     1     2    3      4    5      6     7     8     9     .
+	uint8_t fnd_font[] = {0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x92, 0x82, 0xd8, 0x80, 0x98, 0x7f}; //common 애노드
+#else					//   0     1     2    3      4    5      6     7     8     9     .
+	uint8_t fnd_font[] = {0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d, 0x27, 0x7f, 0x67, 0x80}; //common 캐소드
+#endif
+
+uint8_t big_circle_left[]  = {~0x01, ~0x00, ~0x00, ~0x00, ~0x00, ~0x08, ~0x10, ~0x20};
+uint8_t big_circle_right[] = {~0x00, ~0x01, ~0x02, ~0x04, ~0x08, ~0x00, ~0x00, ~0x00};
 	
+void init_fnd();
+void fnd_min_sec_display();
+void fnd_sec_display();
+void fnd_stopwatch();
+int fnd_main(int time_mode);
+void reset_stopwatch();
+void time_stop();
+void init_fnd_stopwatch();
+
+
+int fnd_main(int time_mode){
 	
-	while(1){
-		fnd_display();
-		_delay_ms(1);
-		ms_count++;
+	switch(time_mode){
+		case 0:
+			fnd_min_sec_display();
+			break;
+		case 1:
+			fnd_sec_display();
+			break;
+		case 2:
+			fnd_stopwatch();
+			break;
+	}
+	
+	_delay_ms(1);
+	ms_count++;
+	
+	if(time_mode == 2){
+		if(stopwatch_ms_count >= 6000){
+			stopwatch_ms_count = 0;
+		}
+		//10ms 100ms 1000ms 10000ms 속도로 변화
+		sec_10 = (ms_count / 100) / 10; // 초의 10의 자리 (0~5)
+		sec_1  = (ms_count / 100) % 10; // 초의 1의 자리 (0~9)
+		ms_10  = (ms_count % 100) / 10;  // 밀리초의 10의 자리 (0~9)
+		ms_1   = (ms_count % 100) % 10;  // 밀리초의 1의 자리 (0~9)
+		
+	}else{
 		if(ms_count >= 1000){
 			ms_count = 0;
 			sec_count++;
@@ -46,14 +87,7 @@ void init_fnd(){
 
 	#endif
 }
-void fnd_display(){
-	#if 1
-						//   0     1     2    3      4    5      6     7     8     9     .
-	uint8_t fnd_font[] = {0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x92, 0x82, 0xd8, 0x80, 0x98, 0x7f}; //common 애노드
-	#else
-						//   0     1     2    3      4    5      6     7     8     9     .
-	uint8_t fnd_font[] = {0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x92, 0x82, 0xd8, 0x80, 0x98, 0x7f}; //common 캐소드
-	#endif
+void fnd_min_sec_display(){
 	
 	static int digit_select = 0; // 자리수 선택
 	
@@ -96,4 +130,149 @@ void fnd_display(){
 	}
 	
 	digit_select = (digit_select + 1) % 4; //다음 표시할 자리 수
+}
+
+void fnd_sec_display(){
+	
+	static int digit_select = 0; // 자리수 선택
+
+	
+	switch(digit_select){
+		case 0: //1단위
+		#if 1
+		FND_DIGHT_PORT = 0x80; //애노드
+		#else
+		FND_DIGHT_PORT = ~0x80; //캐소드
+		#endif
+		FND_DATA_PORT = fnd_font[sec_count % 10]; //0~9까지 출력
+		break;
+		
+		case 1: // 10단위
+		#if 1		
+		FND_DIGHT_PORT = 0x40; //애노드
+		#else
+		FND_DIGHT_PORT = ~0x40; //캐소드
+		#endif
+		FND_DATA_PORT = fnd_font[sec_count/10 % 6]; //0~59까지 출력
+		break;
+		
+		case 2: // 100단위
+		#if 1
+		FND_DIGHT_PORT = 0x20; //애노드
+		#else
+		FND_DIGHT_PORT = ~0x20; //캐소드
+		#endif
+		FND_DATA_PORT = big_circle_right[sec_count % 8];
+		break;
+		
+		case 3: // 1000단위
+		#if 1
+		FND_DIGHT_PORT = 0x10; //애노드
+		#else
+		FND_DIGHT_PORT = ~0x10; //캐소드
+		#endif
+		FND_DATA_PORT = big_circle_left[sec_count % 8];
+		break;
+	}
+	
+	digit_select = (digit_select + 1) % 4;
+	
+}
+
+void fnd_stopwatch(){
+	
+	static int digit_select = 0; // 자리수 선택
+	
+	switch(digit_select){
+		case 0: //1단위
+		#if 1
+		FND_DIGHT_PORT = 0x80; //애노드
+		#else
+		FND_DIGHT_PORT = ~0x80; //캐소드
+		#endif
+		FND_DATA_PORT = fnd_font[ms_1]; //0~9까지 출력
+		break;
+		
+		case 1: // 10단위
+		#if 1
+		FND_DIGHT_PORT = 0x40; //애노드
+		#else
+		FND_DIGHT_PORT = ~0x40; //캐소드
+		#endif
+		FND_DATA_PORT = fnd_font[ms_10]; //0~59까지 출력
+		break;
+		
+		case 2: // 100단위
+		#if 1
+		FND_DIGHT_PORT = 0x20; //애노드
+		#else
+		FND_DIGHT_PORT = ~0x20; //캐소드
+		#endif
+		FND_DATA_PORT = fnd_font[sec_1];
+		break;
+		
+		case 3: // 1000단위
+		#if 1
+		FND_DIGHT_PORT = 0x10; //애노드
+		#else
+		FND_DIGHT_PORT = ~0x10; //캐소드
+		#endif
+		FND_DATA_PORT = fnd_font[sec_10];
+		break;
+	}
+	
+	digit_select = (digit_select + 1) % 4; //다음 표시할 자리 수
+}
+
+void reset_stopwatch(){
+	
+}
+
+void time_stop(){
+	
+}
+
+void init_fnd_stopwatch(){
+	static int digit_select = 0;
+
+	
+	switch(digit_select){
+		case 0: //1단위
+		#if 1
+		FND_DIGHT_PORT = 0x80; //애노드
+		#else
+		FND_DIGHT_PORT = ~0x80; //캐소드
+		#endif
+		FND_DATA_PORT = fnd_font[0];
+		break;
+		
+		case 1: // 10단위
+		#if 1
+		FND_DIGHT_PORT = 0x40; //애노드
+		#else
+		FND_DIGHT_PORT = ~0x40; //캐소드
+		#endif
+		FND_DATA_PORT = fnd_font[0];
+		break;
+		
+		case 2: // 100단위
+		#if 1
+		FND_DIGHT_PORT = 0x20; //애노드
+		#else
+		FND_DIGHT_PORT = ~0x20; //캐소드
+		#endif
+		FND_DATA_PORT = big_circle_right[0];
+		break;
+		
+		case 3: // 1000단위
+		#if 1
+		FND_DIGHT_PORT = 0x10; //애노드
+		#else
+		FND_DIGHT_PORT = ~0x10; //캐소드
+		#endif
+		FND_DATA_PORT = big_circle_left[0];
+		break;
+	}
+	
+	digit_select = (digit_select + 1) % 4;
 }
--- avr/ai2/ATMEGA128A/02.FND_CONTROL_MY/main_my.c
+++ avr/ai2/ATMEGA128A/02.FND_CONTROL_MY/main_my.c
@@ -1,18 +1,46 @@
 /*
- * 02.FND_CONTROL_MY.c
- *
- * Created: 2026-06-12 오후 1:20:56
- * Author : kccistc
- */ 
+* 02.FND_CONTROL_MY.c
+*
+* Created: 2026-06-12 오후 1:20:56
+* Author : kccistc
+*/
 
 #include <avr/io.h>
+#include "fnd_button.h"
+
+extern void init_button();
+extern int get_button(int button_num, int button_pin);
+extern int fnd_main(int time_mode);
+extern void init_fnd();
+extern void reset_stopwatch();
+extern void time_stop();
+extern void init_fnd_stopwatch();
 
 
 int main(void)
 {
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
+	static int time_mode = 0;
+	
+	init_button();
+	init_fnd();
+	
+	while (1){
+		if(get_button(BUTTON0, BUTTON0PIN)){
+			time_mode = (time_mode + 1) % 3;
+			if(time_mode == 2)  init_fnd_stopwatch();
+		}
+		
+		fnd_main(time_mode);
+		
+		if(get_button(BUTTON1, BUTTON1PIN)){
+			//스톱워치 시작 일시정지
+			time_stop();
+		}
+		
+		if(get_button(BUTTON2, BUTTON2PIN)){
+			//스톱워치 리셋   한번더 누르면 시작
+			reset_stopwatch();
+		}
+	}
 }
 
