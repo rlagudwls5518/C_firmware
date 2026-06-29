d1110698e9d69830ad1879bd7a117b06af16bb36
rlagudwls5518 <rlagudwls5518@naver.com>
2026-06-29 17:12:56 +0900

    계산기 과제 추가

--- avr/ai2/ATMEGA128A/11.KEYPAD_MY/calculator.c
+++ avr/ai2/ATMEGA128A/11.KEYPAD_MY/calculator.c
@@ -5,8 +5,14 @@
  *  Author: kccistc
  */ 
 #include <stdio.h>
+#include "queue.h"
 int result = 0;
 int current_num = 0;
+int num1 =0;
+int num2 =0;
+int number_stack[10];
+char op_stack[10];
+
 
 void calculator(int key);
 void print_calculator(int key);
@@ -27,6 +33,9 @@ void calculator(int key){
 		current_num = (current_num * 10) + input_digit;
 	}
 	else{
+		insert_queue(current_num);
+		
+		num1 = current_num;
 		switch(key){
 			
 			case '+':
